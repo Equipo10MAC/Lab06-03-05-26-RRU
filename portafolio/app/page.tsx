60447b2e7b645c47cec98553bce98c935c788313
Rodrigo Rojas Uriostigue <uriostiguerodrigo@gmail.com>
2026-05-05 03:49:09 +0000

    Añadimos proyectos

--- portafolio/app/page.tsx
+++ portafolio/app/page.tsx
@@ -2,6 +2,7 @@ import Header from "./Components/Header/Header";
 import Hero from "./Components/Header/Hero/Hero";
 import Interests from "./Components/Header/Interests/Interests";
 import CV from "./Components/Header/CV/CV";
+import Projects from "./Components/Projects/Projects";
 
 export default function Home() {
   return (
@@ -12,8 +13,8 @@ export default function Home() {
         <Hero />
         <Interests />
         <CV />
+        <Projects />
       </main>
     </>
   );
-}
 }
