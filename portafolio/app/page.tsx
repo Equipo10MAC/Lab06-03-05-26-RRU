500dbfe8feba38fdea55c5a1d02ff2c743b604b0
Rodrigo Rojas Uriostigue <uriostiguerodrigo@gmail.com>
2026-05-05 03:27:43 +0000

    Añadimos Intersts

--- portafolio/app/page.tsx
+++ portafolio/app/page.tsx
@@ -1,10 +1,16 @@
-import Image from "next/image";
 import Header from "./Components/Header/Header";
+import Hero from "./Components/Header/Hero/Hero";
+import Interests from "./Components/Header/Interests/Interests";
 
 export default function Home() {
   return (
     <>
-    <Header />
+      <Header />
+
+      <main>
+        <Hero />
+        <Interests />
+      </main>
     </>
   );
 }
